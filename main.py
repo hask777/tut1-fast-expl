cfefb28a423b30152fe36df5dea1cda55b2374af
hask777 <haskellisp@gmail.com>
2023-03-03 10:53:46 +0300

    l

--- main.py
+++ main.py
@@ -4,4 +4,25 @@ app = FastAPI()
 
 @app.get('/')
 def index():
-    return 'heyy'
+    return {'data':'blog_list'}
+
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all published blogs'}
+
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    # fetch blog with id = id
+    return {'data': id}
+
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data': {'1','2'}}
+    
+     
