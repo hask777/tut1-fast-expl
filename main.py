a1259e0e73a197f12d16371a72fb9b66ec79f2b4
hask777 <haskellisp@gmail.com>
2023-03-05 18:31:47 +0300

    g

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data':'blog_list'}
+@app.get('/blog')
+def index(limit = 10, published: bool = True, sort: Optional[str] = None):
+
+    if published:
+        return {'data':f'{limit} blog_list, {published}'}
+    else:
+        return {'data': f'{limit}'}
 
 
 
-@app.get('/blog/unpublished')
+@app.get('/blog/{id}/unpublished')
 def unpublished():
     return {'data': 'all published blogs'}
 
@@ -22,7 +29,18 @@ def show(id: int):
 
 
 @app.get('/blog/{id}/comments')
-def comments(id):
+def comments(id, limit=10):
     return {'data': {'1','2'}}
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
     
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {'data': f'Blog is created {request.title}'}
+
+# if __name__ == '__main__':
+#     uvicorn.run(app, host='127.0.0.1', port=9000)
      
