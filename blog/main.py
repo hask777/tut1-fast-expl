c12b1f4470a4c9845533aed6b6a36656b3c86161
hask777 <haskellisp@gmail.com>
2023-03-11 10:16:33 +0300

    g

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends, status, Response, HTTPException
+from typing import List
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -33,7 +34,7 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
 
 
 
-@app.get('/blog')
+@app.get('/blog', response_model=List[schemas.Show])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
@@ -63,7 +64,7 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
 
 
 
-@app.get('/blog/{id}', status_code=200)
+@app.get('/blog/{id}', status_code=200, response_model=schemas.Show)
 def show(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
